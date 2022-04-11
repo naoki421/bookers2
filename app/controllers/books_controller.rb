0bb1cd00bc9e1fdfd98cc5854ca5430d45a31087
EC2 Default User <ec2-user@ip-172-31-95-190.ec2.internal>
2022-04-11 04:53:11 +0000

    [Update]edit画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -41,6 +41,13 @@ class BooksController < ApplicationController
     @user = @books.user
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+
   private
 
   def book_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
   def edit
+    @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user)
+    end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
 
   def index
     @user = current_user
